7442f5a74d1246a7f6c038ac61ad76c3df963bfd
童舒婷 <2642749772@qq.com>
2020-06-20 14:27:04 +0800

    更新

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var spider = require("./spider.js")
 
-app.get('*',async function(req, res,next){
-    // 部署到服务器的完整URL
-    var url = 'https://www.mvi-web.cn' + req.originalUrl;
-	console.log('请求的完整URL：'+ url);
-	var content = await spider(url).catch((error)=>{
+app.get('*', async (req, res, next) => {
+	// 部署到服务器的完整URL
+	var url = 'https://www.mvi-web.cn' + req.originalUrl;
+	console.log('请求的完整URL：' + url);
+	var content = await spider(url).catch((error) => {
 		console.log(error);
 		res.send('获取html内容失败');
 		return;
@@ -14,6 +14,6 @@ app.get('*',async function(req, res,next){
 	res.send(content);
 });
 
-app.listen(3000, function () {
-  console.log('预渲染服务已启动！');
+app.listen(3000, () => {
+	console.log('预渲染服务已启动！');
 });
