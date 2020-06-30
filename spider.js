72aed62791c0b3051d261e99dcb7571e255c6c63
lovelin0523 <18356201582@163.com>
2020-06-30 16:37:44 +0800

    更新

--- spider.js
+++ spider.js
@@ -1,36 +1,30 @@
 const puppeteer = require('puppeteer')
+const WSE_LIST = require('./puppeteer-pool.js')
 
-const spider = (url) => {
-	return new Promise((resolve, reject) => {
-		var page = null;
-		var brower = null;
-		//使用puppetee插件启动一个浏览器
-		puppeteer.launch({
-			args: ['--no-sandbox'],
-			dumpio: false, //是否将浏览器进程标准输出和标准错误输入到 process.stdout 和 process.stderr 中
-			headless: true, //是否以无头模式运行浏览器，默认为true，设为false时，可以显示可视化浏览器界面
-		}).then((result) => {
-			brower = result;
-			// 开启一个新页面
-			return brower.newPage();
-		}).then((result) => {
-			page = result;
-			//打开指定网页
-			return page.goto(url, {
-				timeout:60000,//连接超时时间，单位ms
-				waitUntil: 'networkidle0' //网络空闲说明已加载完毕
-			})
-		}).then(() => {
-			return page.evaluate(() => {
-				return document.getElementsByTagName('html')[0].outerHTML;
-			});
-		}).then((result) => {
-			brower.close();
-			resolve(result);
-		}).catch((error) => {
-			reject(error)
-		})
+const spider = async (url) => {
+
+	let tmp = Math.floor(Math.random() * WSE_LIST.length);
+	
+	let browserWSEndpoint = WSE_LIST[tmp];
+	
+	const browser = await puppeteer.connect({
+		browserWSEndpoint
+	});
+	
+	var page = await browser.newPage();
+
+	await page.goto(url, {
+		timeout: 0, //连接超时时间，单位ms
+		waitUntil: 'networkidle0' //网络空闲说明已加载完毕
 	})
+
+	var html = await page.evaluate(() => {
+		return document.getElementsByTagName('html')[0].outerHTML;
+	});
+
+	page.close();
+
+	return html;
 }
 
 
