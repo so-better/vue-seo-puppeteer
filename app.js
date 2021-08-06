fb935fc179f10885df1d94b81eb71ea5c929ae44
lovelin0523 <18356201582@163.com>
2021-08-06 14:29:34 +0800

    更新

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const os = require('os');
 const spider = require("./spider.js")
+const fs = require('fs');//nodejs的fs对象
+const path = require('path');//nodejs的path对象
+const FdOp = require('fd-op');//引入fd-op
+const fdOp = new FdOp(fs,path);//创建实例对象
 
 app.get('*', async (req, res, next) => {
 	// 部署到服务器的完整URL
@@ -12,6 +17,13 @@ app.get('*', async (req, res, next) => {
 		return;
 	});
 	res.send(content);
+	
+	// 访问的爬虫的user-agent
+	let userAgent = req.headers['user-agent'];
+	//将爬虫记录到文本去
+	const filePath = './user_agent.txt';
+	//追加写入内容
+	await fdOp.appendFile(filePath,userAgent+os.EOL+os.EOL);
 });
 
 app.listen(3000, () => {
