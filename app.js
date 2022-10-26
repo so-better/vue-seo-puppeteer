79767f8fd9bf42424560bcc97b00c923785e6542
lovelin0523 <18356201582@163.com>
2022-10-26 10:55:04 +0800

    更新

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
-const os = require('os')
 const spider = require('./spider.js')
-const fs = require('fs') //nodejs的fs对象
-const path = require('path') //nodejs的path对象
-const FdOp = require('fd-op') //引入fd-op
-const fdOp = new FdOp(fs, path) //创建实例对象
 
 app.get('*', async (req, res, next) => {
     // 部署到服务器的完整URL
