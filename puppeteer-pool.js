1cb621a7907424ee56b0f429b8d64fd7b864f2e0
lovelin0523 <18356201582@163.com>
2023-09-21 15:30:39 +0800

    更新

--- puppeteer-pool.js
+++ puppeteer-pool.js
@@ -3,22 +3,14 @@ const MAX_WSE = 2 //启动几个浏览器
 let WSE_LIST = [] //存储browserWSEndpoint列表
 
 ;(async () => {
-    for (let i = 0; i < MAX_WSE; i++) {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: [
-                '--disable-gpu',
-                '--disable-dev-shm-usage',
-                '--disable-setuid-sandbox',
-                '--no-first-run',
-                '--no-sandbox',
-                '--no-zygote',
-                '--single-process'
-            ]
-        })
-        browserWSEndpoint = await browser.wsEndpoint()
-        WSE_LIST.push(browserWSEndpoint)
-    }
+	for (let i = 0; i < MAX_WSE; i++) {
+		const browser = await puppeteer.launch({
+			headless: true,
+			args: ['--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-first-run', '--no-sandbox', '--no-zygote', '--single-process']
+		})
+		browserWSEndpoint = await browser.wsEndpoint()
+		WSE_LIST.push(browserWSEndpoint)
+	}
 })()
 
 module.exports = WSE_LIST
--- spider.js
+++ spider.js
@@ -1,29 +1,20 @@
 const puppeteer = require('puppeteer')
 const WSE_LIST = require('./puppeteer-pool.js')
-
 const spider = async url => {
 	const tmp = Math.floor(Math.random() * WSE_LIST.length)
-
 	const browserWSEndpoint = WSE_LIST[tmp]
-
 	const browser = await puppeteer.connect({
 		browserWSEndpoint
 	})
-
 	const page = await browser.newPage()
-
 	await page.goto(url, {
 		timeout: 0, //连接超时时间，单位ms
 		waitUntil: 'networkidle0' //网络空闲说明已加载完毕
 	})
-
 	const html = await page.evaluate(() => {
 		return document.getElementsByTagName('html')[0].outerHTML
 	})
-
 	await page.close()
-
 	return html
 }
-
 module.exports = spider
