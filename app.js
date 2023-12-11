5c27867f11a92a0b741774f67f7c63ec169f4311
lovelin0523 <18356201582@163.com>
2023-12-11 17:50:23 +0800

    更新

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const spider = require('./spider.js')
 
 app.get('*', async (req, res, next) => {
 	// 部署到服务器的完整URL
-	const url = 'https://www.mvi-web.cn' + req.originalUrl
+	const url = 'https://www.ling0523.cn' + req.originalUrl
 	console.log('请求的完整URL：' + url)
 	const content = await spider(url).catch(error => {
 		console.log(error)
